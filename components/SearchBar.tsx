29ae0bfc39a71da6ebfd1049376ec78efddd61b3
Ayushi diyora <112619773+aayud22@users.noreply.github.com>
2025-07-07 11:03:07 +0530

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { CircleX, Search } from "lucide-react";
+import { Search } from "lucide-react";
 
 interface SearchBarProps {
   searchValue: string;
@@ -15,8 +15,6 @@ export default function SearchBar({
 }: SearchBarProps) {
   return (
     <div className="flex items-center bg-secondary rounded-full px-3.5 w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl transition-all py-1">
-      <Search className="w-5 h-5 text-muted-foreground" />
-
       <input
         type="text"
         value={searchValue}
@@ -35,15 +33,16 @@ export default function SearchBar({
         className="bg-transparent outline-none border-none text-base font-normal focus:ring-0 flex-1 placeholder:text-muted-foreground px-2 min-w-0"
       />
 
-      {searchValue && (
-        <CircleX
-          onClick={() => {
-            setSearchValue("");
-            if (onClear) onClear();
-          }}
-          className="w-5 h-5 cursor-pointer transition-colors hover:text-primary"
-        />
-      )}
+      <div className="flex items-center gap-2">
+        {searchValue && (
+          <>
+            <Search
+              onClick={() => onSearch && onSearch(searchValue)}
+              className="w-5 h-5 cursor-pointer text-muted-foreground hover:text-primary transition-colors"
+            />
+          </>
+        )}
+      </div>
     </div>
   );
 }
